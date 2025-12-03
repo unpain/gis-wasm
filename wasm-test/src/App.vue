ce84f95a80ed326262ccc1f48268bdddb3a23af4
unpain <1733636949a@gmail.com>
2025-12-04 00:30:45 +0800

    feat: add wasm-test to test wasm

---
+++ wasm-test/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import testGeoJSON from './test.json'
+import __wbg_init, { get_feature_length } from '@/pkg/gis_wasm'
+onMounted(async () => {
+  await __wbg_init()
+  console.log(get_feature_length(JSON.stringify(testGeoJSON)))
+})
+</script>
+<template></template>
+<style scoped></style>
